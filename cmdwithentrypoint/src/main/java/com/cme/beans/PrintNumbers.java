cfc75245512acfd77bcaa4f53024f66e5be4e88e
Parushuram <vemulasairam378@gmail.com>
2022-05-21 23:01:39 +0530

    Modified PrintNumber Java file To accept the args instead of Env variables

--- cmdwithentrypoint/src/main/java/com/cme/beans/PrintNumbers.java
+++ cmdwithentrypoint/src/main/java/com/cme/beans/PrintNumbers.java
@@ -5,9 +5,9 @@ public class PrintNumbers {
     static int n=0;
     
     public static void main(String[] args) throws InterruptedException {
-        //n = Integer.parseInt(System.getenv("N"));
-        System.err.println("==============="+n);
-        n = Integer.parseInt("30");
+        n = Integer.parseInt(args[0]);
+        System.err.println("=======Environment variable N value passed is :"+n);
+        
         for(int i=0; i< n; i++) {
             System.out.println("I value is :"+i);
             Thread.sleep(1000);
